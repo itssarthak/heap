e03c265af6e1bcc9e03db6f44987fd710429c3df
Sarthak Chhabra <sarthakc2211@gmail.com>
2021-09-08 22:10:08 +0530

    Create tree from Array

--- index.js
+++ index.js
@@ -13,13 +13,45 @@ function createRow(child) {
     return row;
 }
 
-function main() {
-    let main = document.getElementById('main');
+const getMaxLeaves = (level) => {
+    return Math.pow(2,level);
+}
+
+const prevLevelCount = (level) => {
+    if(level <= 0) return 0;
+    let val = 0;
+    while(level) val += getMaxLeaves(--level);
+    return val;
+}
+
+function createTree(values) {
+    let level = 0;
+    let maxLeaves = getMaxLeaves(level);
+    let row = createRow();
+
+    for(let i=0;i<values.length;i++) {
+        if((i-prevLevelCount(level))==maxLeaves(level)) {
+            main.appendChild(row);
+            row = createRow();
+            level++;
+        }
+        let circle = createCircle(values[i]);
+        row.appendChild(circle);
+    }
+
+    let prevLevelCount = getPrevLevelCount(level);
+    
+
     let temp = createRow(createCircle(10));
     main.appendChild(temp);
     temp = createRow(createCircle(15));
     temp.appendChild(createCircle(20));
     main.appendChild(temp);
+}
+
+function main() {
+    let main = document.getElementById('main');
+    createTree([1,2,3,4,5,6,7,8,9,10])
 }
 
 main();
