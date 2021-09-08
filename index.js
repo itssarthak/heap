e0abb429d283d8c7b93c9764cbea98052f70036b
Sarthak Chhabra <sarthakc2211@gmail.com>
2021-09-08 21:41:54 +0530

    Added Row

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-function createCircle() {
+function createCircle(text) {
     let circle = document.createElement('div');
     circle.className = "circle alignCenter justifyCenter";
+    if(text || text == 0) circle.innerText = text;
     return circle;
 }
 
-let main = document.getElementById('main');
-let temp = createCircle();
-temp.innerText = 10;
-main.appendChild(temp);
+function createRow(child) {
+    let row = document.createElement('div');
+    row.className = "row";
+    if(child) row.appendChild(child);
+    return row;
+}
+
+function main() {
+    let main = document.getElementById('main');
+    let temp = row(createCircle(10));
+    main.appendChild(temp);
+    temp = createRow(createCircle(15));
+    temp.appendChild(createCircle(20));
+    main.appendChild(temp);
+}
+
+main();
+
